f2b9dc843b0a1444b88409d16256f05f0a84aea5
kalandos <kalanda_kalanda@mail.ru>
2019-04-15 09:37:20 +0300

    каланда

--- каланда_1/Program.cs
+++ каланда_1/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace каланда_1
+namespace ConsoleApp1
 {
-    class Program
+    class Firma
     {
-        static void Main(string[] args)
+        private string name;
+        public string Name { get; set; }
+        private string adress;
+        public string Adress { get; set; }
+        private int year_osnova;
+        public int Year_osnova { get; set; }
+        public Firma()
         {
+            Name = "";
+            Adress = "";
+            Year_osnova = 0;
+        }
+        public void Fill()
+        {
+            Console.WriteLine("Введите данные");
+            Name = Console.ReadLine();
+            Adress = Console.ReadLine();
+            Year_osnova = Convert.ToInt32(Console.ReadLine());
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Название фирмы: {Name}, Адрес фирмы: {Adress}, Год основания фирмы: { Year_osnova}");
+        }
+        class program
+        {
+            static void Main(string[] args)
+            {
+                Firma Firma = new Firma();
+                Firma.Fill();
+                Firma.Print();
+                Console.ReadKey();
+            }
         }
     }
 }
+
+
+
+
+
+
