919055eeda7878391137a0435d9867a472bcf04a
kalandos <kalanda_kalanda@mail.ru>
2019-04-15 09:11:10 +0300

    kalandos

---
+++ каланда_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace каланда_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
